0f904140a41e80a266a07a70f28e140ed09d255f
MarcoJunkes <marcojunkes@ufpr.br>
2024-08-28 16:03:52 -0300

    Criação parcial da documentação

--- src/dados-consulta/dados-consulta.service.ts
+++ src/dados-consulta/dados-consulta.service.ts
@@ -5,51 +5,51 @@ import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class DadosConsultaService {
-  private readonly apiKey: string;
+    private readonly apiKey: string;
 
-  constructor(
-    private readonly httpService: HttpService,
-    private readonly configService: ConfigService // Injeta o ConfigService
-  ) {
-    this.apiKey = this.configService.get<string>('OPENWEATHER_API_KEY'); // Obtém a chave da API do .env
-  }
-
-  async processaDadosClima(umidade: string, localizacao: string): Promise<any> {
-    if (!umidade || !localizacao) {
-      return "Dados obrigatórios não preenchidos"
+    constructor(
+        private readonly httpService: HttpService,
+        private readonly configService: ConfigService // Injeta o ConfigService
+    ) {
+        this.apiKey = this.configService.get<string>('OPENWEATHER_API_KEY'); // Obtém a chave da API do .env
     }
 
-    const umidadeNumber = parseInt(umidade);
-    if (isNaN(umidadeNumber)) {
-        return "Umidade fornecida não é um número válido.";
+    async processaDadosClima(umidade: string, localizacao: string): Promise<any> {
+        if (!umidade || !localizacao) {
+            return "Dados obrigatórios não preenchidos"
+        }
+
+        const umidadeNumber = parseInt(umidade);
+        if (isNaN(umidadeNumber)) {
+            return "Umidade fornecida não é um número válido.";
+        }
+
+
+        const [lat, lon] = localizacao.split(' ').map(Number);
+
+        if (isNaN(lat) || isNaN(lon)) {
+            return "Dados obrigatórios não preenchidos ou preenchidos de forma incorreta";
+        }
+
+        try{
+            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`; // Usa a chave da API armazenada
+            const response = await firstValueFrom(this.httpService.get(apiUrl)); // Realiza a chamada HTTP à API
+            const umidadeApi = response.data.main.humidity;
+            const cidade = response.data.name;
+
+            const comparaUmidade = umidadeNumber < umidadeApi; // Realiza a comparação
+            const mensagem = comparaUmidade 
+                ? 'Alerta: A umidade atual em ' + cidade + ' é de ' + umidadeApi + '%, que é maior que o valor informado de ' + umidade 
+                : 'A umidade está dentro do limite informado!'; // Se "comparaUmidade" for verdadeiro, retorna a primeira condição, se não, retorna a segunda
+
+            return {
+                mensagem: mensagem
+            };
+        }catch(error){
+            throw new HttpException(
+                `Falha ao obter dados de clima para a localização (${lat}, ${lon}). Verifique os parâmetros e tente novamente.`,
+                HttpStatus.BAD_REQUEST,
+            ); // Lança uma exceção HTTP com status code 404
+        }
     }
-
-
-    const [lat, lon] = localizacao.split(' ').map(Number);
-
-    if (isNaN(lat) || isNaN(lon)) {
-      return "Dados obrigatórios não preenchidos ou preenchidos de forma incorreta";
-    }
-
-    try{
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`; // Usa a chave da API armazenada
-        const response = await firstValueFrom(this.httpService.get(apiUrl));
-        const umidadeApi = response.data.main.humidity;
-        const cidade = response.data.name;
-
-        const comparaUmidade = umidadeNumber < umidadeApi; // Realiza a comparação
-        const mensagem = comparaUmidade 
-            ? 'Alerta: A umidade atual em ' + cidade + ' é de ' + umidadeApi + '%, que é maior que o valor informado de ' + umidade 
-            : 'A umidade está dentro do limite informado!';
-
-        return {
-            mensagem: mensagem
-        };
-    }catch(error){
-        throw new HttpException(
-            `Falha ao obter dados de clima para a localização (${lat}, ${lon}). Verifique os parâmetros e tente novamente.`,
-            HttpStatus.BAD_REQUEST,
-        );        
-    }
-  }
 }
